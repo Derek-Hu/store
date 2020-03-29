6d63838e47687fb5649bc9e065a1cbe5e237cd81
胡本绿 <benlv.hu@dianrong.com>
2020-03-29 10:02:11 +0800

    移除无用入口

--- config-entry.js
+++ config-entry.js
@@ -1,20 +1,20 @@
 module.exports = [
-  {
-    // 入口JS 文件
-    entry: 'src/preview.js',
-    // 生成的访问路径
-    outPath: '/preview.html',
-    //
-    template: 'public/preview.html',
-  }, {
-    // 入口JS 文件
-    entry: 'src/store.js',
-    // 生成的访问路径
-    outPath: '/store.html',
-  }, {
-    // 入口JS 文件
-    entry: 'src/playground.js',
-    // 生成的访问路径
-    outPath: '/playground.html',
-  },
+  // {
+  //   // 入口JS 文件
+  //   entry: 'src/preview.js',
+  //   // 生成的访问路径
+  //   outPath: '/preview.html',
+  //   //
+  //   template: 'public/preview.html',
+  // }, {
+  //   // 入口JS 文件
+  //   entry: 'src/store.js',
+  //   // 生成的访问路径
+  //   outPath: '/store.html',
+  // }, {
+  //   // 入口JS 文件
+  //   entry: 'src/playground.js',
+  //   // 生成的访问路径
+  //   outPath: '/playground.html',
+  // },
 ];
